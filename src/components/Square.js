4069c3e4de812b1c1c0a0e13865346b92ab47550
marvingradtke <m.gradtke@gmail.com>
2019-11-20 15:31:29 +0100

    added GlobalStyles

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,19 @@
 import React from "react";
+import styled from "@emotion/styled";
+
+const Button = styled.button`
+  background: wheat;
+  border: 2px solid white;
+  float: left;
+  font-size: 24px;
+  font-weight: bold;
+  margin-right: -1px;
+  margin-top: -1px;
+  padding: 0;
+  text-align: center;
+  outline: none;
+`;
 
 export default function Square({ value, onClick }) {
-  return (
-    <button className="square" onClick={onClick}>
-      {value}
-    </button>
-  );
+  return <Button onClick={onClick}>{value}</Button>;
 }
